c0d80fb74b2207524ddc90dc36a5c5f9df781f21
Gustavo <gordono@unsa.edu.pe>
2022-07-21 18:41:40 -0500

    Added CORS integration

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # 3rd-party apps
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
     'allauth',
     'allauth.account',
@@ -70,12 +71,18 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000', # React
+    'http://127.0.0.1:5500' # Live Server
+)
 
 ROOT_URLCONF = 'config.urls'
 
